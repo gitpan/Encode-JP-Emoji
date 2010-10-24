d69c028571680f374d2f8c910d52a9bd3256475e
Yusuke Kawasaki <u-suke@kawa.net>
2010-10-24 06:18:29 +0000

    Import of KAWASAKI/Encode-JP-Emoji-0.60 from CPAN.
    gitpan-cpan-distribution: Encode-JP-Emoji
    gitpan-cpan-version:      0.60
    gitpan-cpan-path:         KAWASAKI/Encode-JP-Emoji-0.60.tar.gz
    gitpan-cpan-author:       KAWASAKI
    gitpan-cpan-maturity:     released

--- lib/Encode/JP/Emoji/FB_EMOJI_TEXT.pm
+++ lib/Encode/JP/Emoji/FB_EMOJI_TEXT.pm
@@ -51,7 +51,7 @@ L<Encode::JP::Emoji>
 
 =head1 COPYRIGHT
 
-Copyright 2009 Yusuke Kawasaki, all rights reserved.
+Copyright 2009-2010 Yusuke Kawasaki, all rights reserved.
 
 =cut
 
@@ -63,7 +63,7 @@ use Encode ();
 use Encode::JP::Emoji::Mapping;
 use Encode::JP::Emoji::Property;
 
-our $VERSION = '0.05';
+our $VERSION = '0.60';
 
 our @EXPORT = qw(
     FB_EMOJI_TEXT
@@ -73,6 +73,7 @@ our $TEXT_FORMAT = '[%s]';
 
 my $ascii  = Encode::find_encoding('us-ascii');
 my $utf8   = Encode::find_encoding('utf8');
+my $stand  = Encode::find_encoding('x-utf8-e4u-unicode');
 my $mixed  = Encode::find_encoding('x-utf8-e4u-mixed');
 my $check  = Encode::FB_XMLCREF();
 
@@ -85,7 +86,12 @@ sub FB_EMOJI_TEXT {
         if ($chr =~ /\p{InEmojiGoogle}/) {
             # google emoji
             $gcode = $code;
-        } elsif ($chr =~ /\p{InEmojiAny}/) {
+        } elsif ($chr =~ /\p{InEmojiUnicode}/) {
+            # unicode emoji
+            my $moct = $utf8->encode(chr $code, $fb);   # Mixed UTF-8 octets
+            my $gstr = $stand->decode($moct, $fb);      # Standard UTF-8 string
+            $gcode = ord $gstr if (1 == length $gstr);
+        } elsif ($chr =~ /\p{InEmojiMixed}/) {
             # others emoji
             my $moct = $utf8->encode(chr $code, $fb);   # Mixed UTF-8 octets
             my $gstr = $mixed->decode($moct, $fb);      # Google UTF-8 string
